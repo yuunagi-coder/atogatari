1f088e9b283f647942cdf5f839c605564576f93e
yuunagi-coder <yuunagi0421@yahoo.co.jp>
2026-08-12 08:18:17 +0900

    fix:トップページにアクセスする際はセッションの確認をスキップする

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,5 +13,9 @@ Rails.application.routes.draw do
   root "tops#top"
   
   resource :user, only: %i[new create]
+  resource :session, only: %i[new create destroy]
+  
+  # パスワードを忘れた場合はMVPリリース後に作成
+  # resources :passwords, param: :token
 
 end
