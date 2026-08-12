40d5b901d8fa658d6a4982202251bc60dde9518e
yuunagi-coder <yuunagi0421@yahoo.co.jp>
2026-08-12 21:43:43 +0900

    feat:ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "もう一度お試しください" }
 
   def new
   end
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      redirect_to root_path
+      redirect_to root_path, notice: "ログインしました"
     else
       redirect_to new_session_path, alert: "メールアドレスまたはパスワードが違います"
     end
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     terminate_session
-    redirect_to new_session_path, status: :see_other
+    redirect_to root_path, status: :see_other, alert: "ログアウトしました"
   end
 end
--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,5 +2,10 @@ class TopsController < ApplicationController
   allow_unauthenticated_access only: %i[ top ]
 
   def top
+    if authenticated?
+      render :map
+    else
+      render :top
+    end
   end
 end
