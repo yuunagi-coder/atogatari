f75729817d7cb3eaec2309f7ecfa6f9e7b0b5a22
yuunagi <yuunagi0421@yahoo.co.jp>
2026-08-17 18:42:55 +0900

    style:rubocopによる修正

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Myapp
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
-    
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
+
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.yml")]
   end
 end
