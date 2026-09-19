cc427cfd0fa7dfd8821988289004698f7868232d
yuunagi <yuunagi0421@yahoo.co.jp>
2026-09-19 16:49:15 +0900

    test:レコードモデルのテストを追加

--- test/factories/records.rb
+++ test/factories/records.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :record do
     spot_name { "test" }
-    created_at { "2026-09-06 08:49:11.419241000 +900"}
+    recorded_at { "2026-09-06 08:49:11.419241000 +900"}
     latitude { 0.35628471e2 }
     longitude { 0.13973876e3 }
     memo { "test" }
